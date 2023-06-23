cf587f90b58c217378536e4f1fdde3aed4772bb2
Sourav <souravbadhan909@gmail.com>
2023-06-22 22:51:48 -0400

    implemented the functions to fetch data with ajax from /tweets and made a form submission using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,30 +8,6 @@
 
 $(document).ready(function(){
 //$('time.timeago').timeago();
-  const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 const createTweetElement = function(tweet) {
   const $tweet = $(`
@@ -73,6 +49,21 @@ const renderTweets = function(tweets) {
   });
 }
 
+const loadTweets = function() {
+  $.ajax({
+    url: 'http://localhost:8080/tweets/',
+    method: 'GET',
+    dataType: 'json',
+    success: function(tweets) {
+      // Passing the tweets to renderTweets function
+      renderTweets(tweets);
+    },
+    error: function(error) {
+      console.error('There is an error in loading the tweets', error);
+    }
+  });
+};
+loadTweets();
 
 $('form').on('submit', function(event) {
 event.preventDefault();
