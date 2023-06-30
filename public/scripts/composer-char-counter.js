73563c44e6a847b630407cb957dfc8ef132dd850
Sourav <souravbadhan909@gmail.com>
2023-06-29 21:18:32 -0400

    Added the function to the html file to count text in the tweet text area

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   console.log("jQuery Loaded");
+  //User input in textarea
   $('.edit textarea').on('input', function() {
     let textLength = $(this).val().length;
     let remaining = 140 - textLength;
-    //let counter = $(this).closest('form').find('.counter');
     $('.counter').text(remaining);
-
+// Add red class to counter if character exceedts its limit.
     if (remaining < 0) {
       $('.counter').addClass('red');
     } else {
